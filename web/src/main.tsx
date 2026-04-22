be9486420cc54350bd29630d2b5848dbbd1e188a
Adityaakr <Adityaakr@users.noreply.github.com>
2026-04-22 20:05:48 +0530

    Update web bootstrap and dependencies

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Buffer } from "buffer";
 import App from "./App";
 import { VaraProviders } from "./providers/VaraProviders";
 import "./styles.css";
 
+if (typeof window !== "undefined" && !("Buffer" in window)) {
+  (window as Window & typeof globalThis & { Buffer: typeof Buffer }).Buffer = Buffer;
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <VaraProviders>
-      <App />
-    </VaraProviders>
-  </React.StrictMode>
+  <VaraProviders>
+    <App />
+  </VaraProviders>
 );
